6f3f9cfaf54277574e6fc7698a0974171e4ae1bf
Eliton Campos <eliton.pecas@gmail.com>
2023-11-19 20:58:53 -0300

    Save All

--- controllers/trf2Controller.php
+++ controllers/trf2Controller.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
 class trf2Controller extends controller
 {
 	public function __construct()
@@ -145,6 +147,7 @@ class trf2Controller extends controller
 	{
 		$u = new Users();
 		$trf2 = new Trf2();
+		$proposta = new Proposta();
 		$u->setLoggedUser();
 
 		if (!$u->hasPermission('trf2')) {
@@ -170,6 +173,44 @@ class trf2Controller extends controller
 		$ENDERECO = addslashes($_POST['ENDERECO']);
 
 		
+		$vlr_atualizado = addslashes($_POST['vlr_atualizado']);
+		$honorarios_perc = addslashes($_POST['honorarios_perc']);
+		$honorarios_vlr = addslashes($_POST['honorarios_vlr_exib']);
+		$ir_perc = addslashes($_POST['ir_perc']);
+		$ir_vlr = addslashes($_POST['ir_vlr']);
+		$rra_perc = addslashes($_POST['rra_perc']);
+		$rra_vlr = addslashes($_POST['rra_vlr']);
+		$pss_perc = addslashes($_POST['pss_perc']);
+		$pss_vlr = addslashes($_POST['pss_vlr_exib']);
+		$vlr_liquido = addslashes($_POST['vlr_liquido_exib']);
+		$proposta_vlr = addslashes($_POST['proposta_vlr']);
+		$proposta_perc = addslashes($_POST['proposta_perc']);
+		$max_perc = addslashes($_POST['max_perc']);
+		$max_vlr = addslashes($_POST['max_vlr_exib']);
+		$id_processo = addslashes($_POST['id_processo']);
+		$tipo = addslashes($_POST['tipo']);
+		$id_tabela = addslashes($_POST['id_tabela']);
+		$calc_id = addslashes($_POST['calc_id']);
+
+
+		$data['proposta'] = $_POST;
+
+
+		ob_start();
+		$this->loadView('proposta', $data);
+		$proposta_pg = ob_get_contents();
+		ob_end_clean();
+
+		$mpdf = new \Mpdf\Mpdf([
+			'mode' => 'utf-8',
+			'margin_left' => 0,
+			'margin_right' => 0,
+			'margin_top' => 0
+		]);
+
+		$mpdf->WriteHTML($proposta_pg);
+		$mpdf->Output();
+
 		if (!empty($_POST['user_id'])) {
 			$user_id = addslashes($_POST['user_id']);
 		} else {
@@ -197,6 +238,26 @@ class trf2Controller extends controller
 			$ENDERECO,
 			$user_id
 		);
+
+		$proposta->update(
+			$calc_id, 
+			$vlr_atualizado, 
+			$honorarios_perc, 
+			$honorarios_vlr, 
+			$ir_perc, $ir_vlr, 
+			$rra_perc, $rra_vlr, 
+			$pss_perc, 
+			$pss_vlr, 
+			$vlr_liquido, 
+			$proposta_perc, 
+			$proposta_vlr, 
+			$max_perc, 
+			$max_vlr, 
+			$id_processo, 
+			$tipo, 
+			$id_tabela
+		);
+
 		header("Location: " . BASE_URL . "trf2/edit/".$ID);
 	}
 
